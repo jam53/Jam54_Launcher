0ffd139b54792a845937396378834a4af23f4685
jam54 <git@jam54.com>
2021-08-18 17:16:43 +0200

    messing around with the UIToolkit script

--- Assets/Jam54Launcher/Scripts/MainMenu.cs
+++ Assets/Jam54Launcher/Scripts/MainMenu.cs
@@ -5,26 +5,41 @@ using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
-    public Button GamesButton;
+    //Objects
+    public Button Games_Button;
+    public ScrollView GamesPrograms_ScrollView;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
         //Get the root visual element that contains all the objects we need
         VisualElement rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
 
         //Find the object of type 'Button' with the name 'Games_Button' in the root visual element
-        GamesButton = rootVisualElement.Q<Button>("Games_Button");
+        Games_Button = rootVisualElement.Q<Button>("Games_Button");
 
-        #region Add corresponding methods to elements
-        GamesButton.clicked += GamesButtonPressed;
+        GamesPrograms_ScrollView = rootVisualElement.Q<ScrollView>("GamesPrograms_ScrollView");
+
+        #region Add corresponding methods to the elements
+        Games_Button.clicked += GamesButtonPressed;
         #endregion
+    }
+
+    private void Start()
+    {
+        GamesPrograms_ScrollView.touchScrollBehavior = ScrollView.TouchScrollBehavior.Elastic;
+        Debug.Log(GamesPrograms_ScrollView.childCount);
     }
 
     private void GamesButtonPressed()
     {
         Debug.Log("games button pressed");
-        GamesButton.style.display = DisplayStyle.Flex;
+        GamesPrograms_ScrollView.Clear();
+        for (int i = 0; i < 20; i++)
+        {
+            GamesPrograms_ScrollView.Add(Games_Button);
+
+        }
+        Games_Button.style.display = DisplayStyle.Flex;
     }
 }
 
