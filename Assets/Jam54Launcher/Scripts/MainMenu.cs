6cf5776e030c60ef0d9c42da2e38c7237b3b6b31
jam54 <git@jam54.com>
2021-08-22 17:17:15 +0200

    Added the download/update icon to the apps on the main menu

--- Assets/Jam54Launcher/Scripts/MainMenu.cs
+++ Assets/Jam54Launcher/Scripts/MainMenu.cs
@@ -46,6 +46,9 @@ private void GamesButtonPressed()
         {
             b.style.height = val;
         }).Ease(Easing.OutBounce);
+        AudioDataLoadState je 
+            //als je op het fototje zelf klikt op main menu, start je de game
+            //druk je op de tekst, dan opent de store page
 
         
 
