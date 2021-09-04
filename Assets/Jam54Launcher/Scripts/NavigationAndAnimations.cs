d4585ab8de12641386653637fa1f62cd2f6a723c
jam54 <git@jam54.com>
2021-09-04 16:36:50 +0200

    Added scripts

---
+++ Assets/Jam54Launcher/Scripts/NavigationAndAnimations.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UIElements.Experimental;
+
+//This script handles all the animations when clicking on certain elements and makes it possible to navigate in the app.
+//In other words, the logic behind clicking on a button in order to open a new tab and close the current one is handled here.
+//This script may also call certain methods from order scripts, to execute an action
+public class NavigationAndAnimations : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
