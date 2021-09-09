b4824739ffb704159682a0915a3df804d16d802b
jam53 <deniz.callens@outlook.com>
2021-09-09 12:01:30 +0200

    Added ScriptableObjects with AppInfo for the product page

---
+++ Assets/Jam54Launcher/Scripts/AppsInfo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New AppsInfo", menuName = "AppInfo")]
+public class AppsInfo : ScriptableObject
+{
+    public string Title;
+
+    public Texture2D Icon;
+
+    public Color BackgroundColor;
+
+    public string LatestUpdate;
+
+    public string ReleaseDate;
+
+    public bool Android;
+
+    public bool Windows;
+
+    public string description;
+}
