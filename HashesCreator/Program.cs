d5c3e05bf001b9ec24376252def3edc8efbd16bc
jam54 <git@jam54.com>
2021-09-13 23:22:11 +0200

    Created a new C3 Console .NET core Visual Studio project called "HashesCreator". This creates hashes of all the files which will be used when updating the apps/programs

---
+++ HashesCreator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashesCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
