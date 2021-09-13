81053818f5da533fa362cececd8d9388a2f5840d
jam54 <git@jam54.com>
2021-09-13 23:07:45 +0200

    Fixed an issue that the Jam54LauncherStarter wouldn't start the Jam54Launcher because it got stuck connecting to/downloading from GitHub

--- Jam54LauncherStarter/MainWindow.xaml.cs
+++ Jam54LauncherStarter/MainWindow.xaml.cs
@@ -15,8 +15,6 @@ namespace Jam54LauncherStarter
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool NewUpdateAvailable;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -54,19 +52,12 @@ namespace Jam54LauncherStarter
         //Check for updates with Squirrel, and install them if there are any
         private async Task CheckForUpdates()
         {
-            using (var manager = new UpdateManager(@"C:\Projects\MyApp\Releases"))//Where does Squirrel have to check for the update files
-            {
-                var updateInfo = await manager.CheckForUpdate();
-
-                if (updateInfo.ReleasesToApply.Any())//If there is a new update, this returns true
-                {
-                    NewUpdateAvailable = true;
-                }
-
-                await manager.UpdateApp();//If there is an update, this will update the Jam54Launcher, and the next time the Jam54Launcher is opened,
-                //the changes will apply. We call this method regardless of whether there is an update or not. If there isn't a new update, it
-                //doesn't doe anything
-            }
+            var manager = await UpdateManager.GitHubUpdateManager(@"https://github.com/jamhorn/vamos");//Where does Squirrel have to check for the update files
+            
+            await manager.UpdateApp();//If there is an update, this will update the Jam54Launcher, and the next time the Jam54Launcher is opened,
+            //the changes will apply. We call this method regardless of whether there is an update or not. If there isn't a new update, it
+            //doesn't do anything
+            
         }
 
         //We do this before we launch the Jam54Launcher, other wise it will crash since it can't find the files it needs
@@ -127,32 +118,19 @@ namespace Jam54LauncherStarter
         //Launch the Jam54Launcher
         private async Task LaunchJam54LauncherMain()
         {
-            if (NewUpdateAvailable)//If there was a new update available, that means that the new version has been installed,
-                //but the changes havent been applied. They only get applied during the next restart. But since we want to have the most recent/
-                //updated version immediately, we relaunch the application
+            try
+            //Should this fail, just go to the location under %appdata%, since squirrel always stores the stuff it installs there
+            {
+                string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                Process.Start(path + @"\Jam54LauncherApp\Jam54LauncherMain.exe");
+            }
+            catch (Exception ex)
             {
                 string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"\Local\Jam54Launcher\Update.exe");
-                Process.Start(path, "--processStart Jam54Launcher.exe"); //Start a new instance of this program
-                Environment.Exit(0); //Close the current instance
+                Process.Start(path, "--processStart Jam54Launcher.exe");
             }
 
-            else //If there isn't a new update, of which we want to apply the changes immediately, then we can launch the Jam54Launcher right away/straight away
-            {
-                try
-                    //Should this fail, just go to the location under %appdata%, since squirrel always stores the stuff it installs there
-                {
-                    string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                    Process.Start(path + @"\Jam54LauncherApp\Jam54LauncherMain.exe");
-                }
-                catch (Exception ex)
-                {
-                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"\Local\Jam54Launcher\Update.exe");
-                    Process.Start(path, "--processStart Jam54Launcher.exe");
-                }
-
-                Environment.Exit(0); //exit the program
-            }
-
+            Environment.Exit(0); //exit the program
         }
 
 
