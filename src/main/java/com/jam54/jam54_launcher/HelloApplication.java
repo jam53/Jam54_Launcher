75f1d0abca5db2ffd593cb378f0f672f166d142b
jam54 <git@jam54.com>
2022-07-21 18:25:30 +0200

    Added the background, topbar and leftbar

--- src/main/java/com/jam54/jam54_launcher/HelloApplication.java
+++ src/main/java/com/jam54/jam54_launcher/HelloApplication.java
@@ -12,9 +12,9 @@ public class HelloApplication extends Application
     @Override
     public void start(Stage stage) throws IOException
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Jam54 Launcher");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/jam54/jam54_launcher/HelloController.java
+++ src/main/java/com/jam54/jam54_launcher/HelloController.java
@@ -5,12 +5,5 @@ import javafx.scene.control.Label;
 
 public class HelloController
 {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick()
-    {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
