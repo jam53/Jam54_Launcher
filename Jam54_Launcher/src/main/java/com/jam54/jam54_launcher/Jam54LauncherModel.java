8474e7b43783edb86e6827e7be5877ae54627f6e
jam53 <deniz.callens@outlook.com>
2022-08-06 21:10:30 +0200

    Added some missing comments

--- Jam54_Launcher/src/main/java/com/jam54/jam54_launcher/Jam54LauncherModel.java
+++ Jam54_Launcher/src/main/java/com/jam54/jam54_launcher/Jam54LauncherModel.java
@@ -6,6 +6,12 @@ import javafx.beans.Observable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Whilst the {@link com.jam54.jam54_launcher.SaveLoad.Jam54LauncherData} is used to have persistent data, by saving and loading data from the disk.
+ *
+ * This class is used to store data during runtime. It is the model in the MVC pattern, therefore when we change the value of a variable,
+ * the listeners will be notified that a variable's value inside the model has been changed
+ */
 public class Jam54LauncherModel implements Observable
 {
     private final List<InvalidationListener> listenerList;
